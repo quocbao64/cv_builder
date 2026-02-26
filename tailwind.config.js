80a65cda5ac8b55ac6f95cae7e4eb90659037d80
baobkt <bao@bachkhoa.tech>
2026-02-26 10:36:43 +0700

    config: Add container configuration to the Tailwind CSS theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ export default {
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   safelist: ['p-6', 'p-10', 'p-14'],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       borderRadius: {
         lg: 'var(--radius)',
